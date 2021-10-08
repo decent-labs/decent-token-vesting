4dba0dc584580cecc1493c20a417a675c21a8cd7
Adam Gall <adam@decentlabs.io>
2021-10-07 23:42:54 -0400

    Filter all vests into created and claimable

--- src/data/data.ts
+++ src/data/data.ts
@@ -6,21 +6,29 @@ import {
   Vest,
   useAllVests,
   useVestsLoading,
+  useMyCreatedVests,
+  useMyClaimableVests,
 } from './vests';
 
 export interface Data {
   vests: {
     all: Vest[],
+    myCreated: Vest[],
+    myClaimable: Vest[],
   },
 };
 
 function useSystemData() {
   const [generalTokenVestingContract, generalTokenVestingDeploymentBlock] = useGeneralTokenVestingContract();
   const [allVests, allVestsLoading] = useAllVests(generalTokenVestingContract, generalTokenVestingDeploymentBlock);
+  const myCreatedVests = useMyCreatedVests(allVests);
+  const myClaimableVests = useMyClaimableVests(allVests);
 
   const data: Data = {
     vests: {
       all: allVests,
+      myCreated: myCreatedVests,
+      myClaimable: myClaimableVests,
     },
   };
 
