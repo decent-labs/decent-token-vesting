53973abeca9fda2e08e3205bf2a34e2a0b737372
Adam Gall <adam@decentlabs.io>
2021-10-07 23:04:34 -0400

    Create some left menu items and match them

--- src/components/structure/Body.tsx
+++ src/components/structure/Body.tsx
@@ -1,9 +1,23 @@
+import { Switch, Route, Redirect } from "react-router-dom";
 import AllTokenVests from '../AllTokenVests';
 
 function Body() {
   return (
     <div className="flex-grow pl-4 pt-4">
-      <AllTokenVests />
+      <Switch>
+        <Route path="/all-vests">
+          <AllTokenVests />
+        </Route>
+        <Route path="/my-created-vests">
+          <AllTokenVests />
+        </Route>
+        <Route path="/my-claimable-vests">
+          <AllTokenVests />
+        </Route>
+        <Route path="/">
+          <Redirect to="/all-vests" />
+        </Route>
+      </Switch>
     </div>
   )
 }
--- src/components/structure/LeftMenu.tsx
+++ src/components/structure/LeftMenu.tsx
@@ -1,22 +1,39 @@
+import { Link, useRouteMatch } from 'react-router-dom';
+
 function MenuItem({
   title,
+  to,
 }: {
-  title: string
+  title: string,
+  to: string,
 }) {
+  const match = useRouteMatch(to);
+
   return (
-    <div className="py-4">
-      {title}
-    </div>
+    <Link to={to}>
+      <div className={`py-2 ${match ? "font-bold" : ""}`}>
+        {title}
+      </div>
+    </Link>
   );
 }
 
 function LeftMenu() {
   return (
-    <div className="border-r pt-4 pr-4">
-      <div className="-my-4">
-        <MenuItem title="All vests" />
-        <MenuItem title="My created vests" />
-        <MenuItem title="My claimable vests" />
+    <div className="border-r pt-4 pr-4 w-44">
+      <div className="-my-2">
+        <MenuItem
+          title="All vests"
+          to="/all-vests"
+        />
+        <MenuItem
+          title="My created vests"
+          to="/my-created-vests"
+        />
+        <MenuItem
+          title="My claimable vests"
+          to="/my-claimable-vests"
+        />
       </div>
     </div>
   );
