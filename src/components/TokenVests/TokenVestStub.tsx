2ec60c490cd273e26953ad15ee68898c1561c016
Adam Gall <adam@decentlabs.io>
2021-10-08 00:16:47 -0400

    Make TokenVestStub link to the Detail page

--- src/components/TokenVests/TokenVestStub.tsx
+++ src/components/TokenVests/TokenVestStub.tsx
@@ -1,4 +1,5 @@
 import { ethers } from 'ethers';
+import { Link } from 'react-router-dom';
 import { Vest } from '../../data/vests';
 
 function TokenVestStub({
@@ -8,15 +9,17 @@ function TokenVestStub({
 }) {
   return (
     <div className="mb-4">
-      <div>token: {vest.token.name} ({vest.token.symbol}) {vest.token.instance.address}</div>
-      <div>creator: {vest.creator}</div>
-      <div>beneficiary: {vest.beneficiary}</div>
-      <div>start: {vest.start.toLocaleString()}</div>
-      <div>end: {vest.end.toLocaleString()}</div>
-      <div>total amount: {ethers.utils.formatUnits(vest.totalAmount, vest.token.decimals)}</div>
-      <div>total vested amount: {ethers.utils.formatUnits(vest.totalVestedAmount, vest.token.decimals)}</div>
-      <div>released amount: {ethers.utils.formatUnits(vest.releasedAmount, vest.token.decimals)}</div>
-      <div>releasable amount: {ethers.utils.formatUnits(vest.releasableAmount, vest.token.decimals)}</div>
+      <Link to={`${vest.token.instance.address}-${vest.beneficiary}`}>
+        <div>token: {vest.token.name} ({vest.token.symbol}) {vest.token.instance.address}</div>
+        <div>creator: {vest.creator}</div>
+        <div>beneficiary: {vest.beneficiary}</div>
+        <div>start: {vest.start.toLocaleString()}</div>
+        <div>end: {vest.end.toLocaleString()}</div>
+        <div>total amount: {ethers.utils.formatUnits(vest.totalAmount, vest.token.decimals)}</div>
+        <div>total vested amount: {ethers.utils.formatUnits(vest.totalVestedAmount, vest.token.decimals)}</div>
+        <div>released amount: {ethers.utils.formatUnits(vest.releasedAmount, vest.token.decimals)}</div>
+        <div>releasable amount: {ethers.utils.formatUnits(vest.releasableAmount, vest.token.decimals)}</div>
+      </Link>
     </div>
   );
 }
