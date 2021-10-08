28850e9124f8e2d4b1a43686098b729d8ffd7d6c
Adam Gall <adam@decentlabs.io>
2021-10-08 00:41:17 -0400

    Make header title a link to home

--- src/components/structure/Header.tsx
+++ src/components/structure/Header.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import { useWeb3 } from '../../web3';
 import { connect } from '../../web3/providers';
 import Button from '../ui/Button';
@@ -9,7 +10,9 @@ function Header() {
     <div className="py-4 bg-gray-100 border-b">
       <div className="container flex flex-col sm:flex-row sm:justify-between sm:items-center">
         <div className="text-4xl mr-2 mb-4 sm:mb-0">
-          General Token Vesting
+          <Link to="/">
+            General Token Vesting
+          </Link>
         </div>
         <div className="sm:text-right">
           {!account && (
