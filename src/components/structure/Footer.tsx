edd8cf3b048d3c0035d920d74f7c7ea6148bd680
Adam Gall <adam@decentlabs.io>
2021-10-07 21:39:38 -0400

    Make version identifier even smaller

--- src/components/structure/Footer.tsx
+++ src/components/structure/Footer.tsx
@@ -7,7 +7,7 @@ function Footer() {
   return (
     <div className="py-2 mt-4 border-t bg-gray-100 text-sm">
       <div className="container flex flex-col sm:flex-row sm:justify-between sm:items-center items-end">
-        <div className="font-mono">v{packageJson.version}{process.env.REACT_APP_GIT_HASH && `+${process.env.REACT_APP_GIT_HASH}`}</div>
+        <div className="font-mono text-xs">v{packageJson.version}{process.env.REACT_APP_GIT_HASH && `+${process.env.REACT_APP_GIT_HASH}`}</div>
         <div>{web3.networkName && `${web3.networkName} via `}{web3.providerName}</div>
       </div>
     </div>
