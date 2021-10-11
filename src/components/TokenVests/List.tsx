c4d075a1e948cb23fee29e077627f78252105d71
Adam Gall <adam@decentlabs.io>
2021-10-11 11:45:27 -0400

    Show loading message on Lists screens when no vests are there

--- src/components/TokenVests/List.tsx
+++ src/components/TokenVests/List.tsx
@@ -1,6 +1,38 @@
+import { useData } from '../../data';
 import { Vest } from '../../data/vests';
 import Title from '../ui/Title';
 import Stub from './Stub';
+
+function VestsList({
+  vests,
+}: {
+  vests: Vest[],
+}) {
+  const { loading } = useData();
+
+  if (vests.length === 0) {
+    if (loading) {
+      return (
+        <div>loading token vests</div>
+      );
+    } else {
+      return (
+        <div>no token vests</div>
+      );
+    }
+  }
+
+  return (
+    <div>
+      {vests.map(v => (
+        <Stub
+          key={v.id}
+          vest={v}
+        />
+      ))}
+    </div>
+  );
+}
 
 function List({
   title,
@@ -12,14 +44,7 @@ function List({
   return (
     <div>
       <Title title={title} />
-      <div>
-        {vests.map(v => (
-          <Stub
-            key={v.id}
-            vest={v}
-          />
-        ))}
-      </div>
+      <VestsList vests={vests} />
     </div>
   );
 }
