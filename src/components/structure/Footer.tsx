f19d02b5c011b4e40c741845413bec9aaae056ea
Adam Gall <adam@decentlabs.io>
2021-10-12 17:14:30 -0400

    But show syncing status in the footer

--- src/components/structure/Footer.tsx
+++ src/components/structure/Footer.tsx
@@ -5,17 +5,21 @@ import EtherscanLink from '../ui/EtherscanLink';
 
 function Footer() {
   const { networkName, providerName } = useWeb3();
-  const { contracts: { generalTokenVesting } } = useData();
+  const { contracts: { generalTokenVesting }, loading } = useData();
 
   return (
     <div className="bread py-2 border-t text-xs sm:text-sm">
-      <div className="container flex flex-col-reverse sm:flex-row sm:justify-between sm:items-center items-end">
-        <div className="flex items-baseline">
+      <div className="container flex flex-col-reverse sm:flex-row sm:justify-between sm:items-center">
+        <div className="text-right sm:text-left">
           <div className="font-mono text-xs">v{packageJson.version}{process.env.REACT_APP_GIT_HASH && `+${process.env.REACT_APP_GIT_HASH}`}</div>
-          <div className="px-2">|</div>
           <div><EtherscanLink address={`${generalTokenVesting?.address}#code`}>view on etherscan</EtherscanLink></div>
         </div>
-        <div>{networkName && `${networkName} via `}{providerName}</div>
+        <div className="text-right">
+          {loading && (
+            <div>...syncing in background</div>
+          )}
+          <div>{networkName && `${networkName} via `}{providerName}</div>
+        </div>
       </div>
     </div>
   );
