cbee1c72babb05c41eac56e2841746e82816ce4e
Adam Gall <adam@decentlabs.io>
2021-10-12 02:42:54 -0400

    Display time since ending on Over and Claimable and Completed cards

--- src/components/TokenVests/Card/Completed.tsx
+++ src/components/TokenVests/Card/Completed.tsx
@@ -1,4 +1,7 @@
+import { BigNumber } from 'ethers';
+import { useData } from '../../../data';
 import { Vest } from '../../../data/vests';
+import useFormattedDuration from '../../../hooks/useFormattedDuration';
 import { Property } from '../../ui/Properties';
 
 function Completed({
@@ -6,10 +9,15 @@ function Completed({
 }: {
   vest: Vest,
 }) {
+  const { currentTime } = useData();
+
+  const formattedTimeSinceEnd = useFormattedDuration(BigNumber.from(currentTime - vest.end));
+
   return (
     <div>
       <Property title="ended on">
         <div>{new Date(vest.end * 1000).toLocaleString()}</div>
+        <div>{formattedTimeSinceEnd} ago</div>
       </Property>
     </div>
   );
--- src/components/TokenVests/Card/OverAndClaimable.tsx
+++ src/components/TokenVests/Card/OverAndClaimable.tsx
@@ -1,5 +1,8 @@
+import { BigNumber } from 'ethers';
+import { useData } from '../../../data';
 import { Vest } from '../../../data/vests';
 import useDisplayAmount from '../../../hooks/useDisplayAmount';
+import useFormattedDuration from '../../../hooks/useFormattedDuration';
 import { Property, AmountProperty } from '../../ui/Properties';
 
 function OverAndClaimable({
@@ -7,12 +10,16 @@ function OverAndClaimable({
 }: {
   vest: Vest,
 }) {
+  const { currentTime } = useData();
+
   const claimableAmountDisplay = useDisplayAmount(vest.claimableAmount, vest.token.decimals, true);
+  const formattedTimeSinceEnd = useFormattedDuration(BigNumber.from(currentTime - vest.end));
 
   return (
     <div>
       <Property title="ended on">
         <div>{new Date(vest.end * 1000).toLocaleString()}</div>
+        <div>{formattedTimeSinceEnd} ago</div>
       </Property>
       <AmountProperty
         title="claimable amount"
