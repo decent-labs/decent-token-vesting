ce4028291b3518c07e045308f9391ec252286513
Adam Gall <adam@decentlabs.io>
2021-10-13 14:13:21 -0400

    Small footer tweaks

--- src/components/structure/Footer.tsx
+++ src/components/structure/Footer.tsx
@@ -10,22 +10,22 @@ function Footer() {
 
   const [emoji, setEmoji] = useState("");
   useEffect(() => {
-    const emojis = ["🦺", "❤️", "🖤", "💜", "😘", "😻", "💖", "🎉", "✨", "🔥", "❤️‍🔥", "☠️", "🥰", "👻", "💋", "🎃", "👽", "🤌", "💪", "👌", "👁", "🧠", ":heart:", "🌝", "🌚", "🌈", "🌙", "☃️", "🍑", "🍆", "🎱"];
+    const emojis = ["🦺", "❤️", "🖤", "💜", "😘", "😻", "💖", "🎉", "✨", "🔥", "❤️‍🔥", "☠️", "🥰", "👻", "💋", "🎃", "👽", "🤌", "💪", "👌", "👁", "🧠", "🌝", "🌚", "🌈", "🌙", "☃️", "🍑", "🍆", "🎱"];
     setEmoji(emojis[Math.floor(Math.random() * emojis.length)]);
   }, []);
 
   return (
     <div className="bread py-2 border-t text-xs sm:text-sm">
-      <div className="container flex flex-col-reverse sm:flex-row sm:justify-between sm:items-center">
+      <div className="container flex flex-col-reverse sm:flex-row sm:justify-between sm:items-end">
         <div className="text-right sm:text-left">
           <div className="font-mono text-xs">v{packageJson.version}{process.env.REACT_APP_GIT_HASH && `+${process.env.REACT_APP_GIT_HASH}`}</div>
           {generalTokenVesting && <div><EtherscanLink address={`${generalTokenVesting.address}#code#L498`}>view on etherscan</EtherscanLink></div>}
-          <div>made with {emoji} by <a href="https://decentlabs.io" target="_blank" rel="noreferrer">decent labs</a> in miami</div>
+          <div>made with <span className="mr-1">{emoji}</span> by <a href="https://decentlabs.io" target="_blank" rel="noreferrer">decent labs</a> in miami 🌴</div>
         </div>
         <div className="text-right">
           <button onClick={() => resetApp()}>reset app</button>
           {loading && (
-            <div>...syncing in background</div>
+            <div>...syncing in background 🙈</div>
           )}
           <div>{networkName && `${networkName} via `}{providerName}</div>
         </div>
